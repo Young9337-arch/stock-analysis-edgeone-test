1e2185f2050d8378b6ea58a0d5154e9ce683e4f1
shuaiyang2000-phy <yangs15079754206@163.com>
2026-07-23 22:44:47 +0800

    Restore EdgeOne API route discovery

---
+++ cloud-functions/api/[[default]].js
@@ -0,0 +1,4 @@
+// EdgeOne Makers discovers Node functions from cloud-functions/api.
+// The handler itself is pre-bundled at the functions root so it contains no
+// runtime-relative imports when EdgeOne packages this route.
+export { default } from '../[[default]].js'
